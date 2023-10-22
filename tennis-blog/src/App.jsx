dd80990766c31a43a6be8344bc1a9e4325bd9190
Tyler <tgallup@berkeley.edu>
2023-10-21 18:44:54 -0700

    First Copy of Website

---
+++ tennis-blog/src/App.jsx
@@ -0,0 +1,27 @@
+// import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Article from "./pages/Article";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/article",
+    element: <Article/>,
+  },
+  ],
+)
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  );
+}
+
+export default App;
---
+++ tennis-blog/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+// import "./navbar.css";
+
+export default function NavBar() {
+  return (
+  <div className="navbar">
+    <ul>
+      <li>
+        <a href='../pages/home'>Home</a>
+      </li>
+      <li>
+        <a href='../pages/article'>Article</a>
+      </li>
+    </ul>
+  </div>
+  );
+}
