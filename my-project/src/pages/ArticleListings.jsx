e9a18f1ec51518c25f68eedb2b8421b3d3566506
Tyler <tgallup@berkeley.edu>
2023-10-25 06:26:29 -0700

    Couple bug fixes

--- my-project/src/pages/ArticleListings.jsx
+++ my-project/src/pages/ArticleListings.jsx
@@ -3,9 +3,9 @@ import FeaturedBlog from "../components/FeaturedBlog";
 function ArticleListings({title}) {
   return (
     <div className="p-5">
-      <h1 className="text-center">{title}</h1>
-      <div className="flex items-center">
-        <div className="w-2/5">
+      <h1 className="text-center pb-5">{title}</h1>
+      <div className="flex justify-center">
+        <div className="">
         <FeaturedBlog category={"WTA"} title="Blog Post Title" author="Author" date="Date Posted" image="https://cdn.britannica.com/29/234629-138-ADD4F54E/who-is-Serena-Williams.jpg?w=800&h=450&c=crop" />
         <FeaturedBlog category={"WTA"} title="Blog Post Title" author="Author" date="Date Posted" image="https://cdn.britannica.com/29/234629-138-ADD4F54E/who-is-Serena-Williams.jpg?w=800&h=450&c=crop"/>
         <FeaturedBlog category={"WTA"} title="Blog Post Title" author="Author" date="Date Posted" image="https://cdn.britannica.com/29/234629-138-ADD4F54E/who-is-Serena-Williams.jpg?w=800&h=450&c=crop"/>
@@ -13,7 +13,7 @@ function ArticleListings({title}) {
         <div className="bg-white w-2">
 
         </div>
-        <div className="w-2/5">
+        <div className="">
         <FeaturedBlog category={"WTA"} title="Blog Post Title" author="Author" date="Date Posted" image="https://cdn.britannica.com/29/234629-138-ADD4F54E/who-is-Serena-Williams.jpg?w=800&h=450&c=crop" />
         <FeaturedBlog category={"WTA"} title="Blog Post Title" author="Author" date="Date Posted" image="https://cdn.britannica.com/29/234629-138-ADD4F54E/who-is-Serena-Williams.jpg?w=800&h=450&c=crop"/>
         <FeaturedBlog category={"WTA"} title="Blog Post Title" author="Author" date="Date Posted" image="https://cdn.britannica.com/29/234629-138-ADD4F54E/who-is-Serena-Williams.jpg?w=800&h=450&c=crop"/>
